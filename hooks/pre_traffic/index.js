305943083bf2c8e459b190b8f5d917fa60fe8b3e
lbassin <laurent@bassin.info>
2020-10-05 18:00:01 -0400

    Add travis + deploy

---
+++ hooks/pre_traffic/index.js
@@ -0,0 +1,56 @@
+const AWS = require('aws-sdk');
+const codedeploy = new AWS.CodeDeploy({ apiVersion: '2014-10-06' });
+const lambda = new AWS.Lambda();
+
+exports.handler = (event, context, callback) => {
+    console.log("Entering PreTraffic Hook!");
+
+    const deploymentId = event.DeploymentId;
+    const lifecycleEventHookExecutionId = event.LifecycleEventHookExecutionId;
+
+    const functionToTest = process.env.NewVersion;
+    console.log("BeforeAllowTraffic hook tests started");
+    console.log("Testing new function version: " + functionToTest);
+
+    const lambdaParams = {
+        FunctionName: functionToTest,
+        Payload: "{\"option\": \"time\"}",
+        InvocationType: "RequestResponse"
+    };
+
+    let lambdaResult = "Failed";
+    lambda.invoke(lambdaParams, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            lambdaResult = "Failed";
+        } else {
+            const result = JSON.parse(data.Payload);
+            console.log("Result: " + JSON.stringify(result));
+            console.log("statusCode: " + result.statusCode);
+
+            if (result.statusCode != "400") {
+                console.log("Validation succeeded");
+                lambdaResult = "Succeeded";
+            } else {
+                console.log("Validation failed");
+            }
+
+            const params = {
+                deploymentId: deploymentId,
+                lifecycleEventHookExecutionId: lifecycleEventHookExecutionId,
+                status: lambdaResult
+            };
+
+            codedeploy.putLifecycleEventHookExecutionStatus(params, function (err, data) {
+                if (err) {
+                    console.log("CodeDeploy Status update failed");
+                    console.log(err, err.stack);
+                    callback("CodeDeploy Status update failed");
+                } else {
+                    console.log("CodeDeploy status updated successfully");
+                    callback(null, "CodeDeploy status updated successfully");
+                }
+            });
+        }
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 exports.handle = async (event) => {
+
+    console.log('### Invoked');
+    console.log(event);
+
+    return buildResponse({version: 3}, 200);
+
+    /*
     let containerConfigs = [];
 
     try {
@@ -7,14 +14,15 @@ exports.handle = async (event) => {
         return buildResponse({ error: 'Request does not contain a valid JSON' }, 400)
     }
 
-    return buildResponse({ count: containerConfigs.length }, 200)
+    return buildResponse({ count: containerConfigs.length, foo: 'bar' }, 200)
+     */
 }
 
 function buildResponse(data, status, headers = []) {
     return {
         statusCode: status,
         body: JSON.stringify(data),
-        headers: headers,
+        headers: {},
         isBase64Encoded: false
     }
 }
